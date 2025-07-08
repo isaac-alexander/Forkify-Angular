9daab8906f14af9c49559aca0a687fd17c3c99da
Isaac Alexander <Isaacalexander81@gmail.com>
2025-07-08 02:25:36 +0100

    src/app/components/side/side.ts

--- src/app/components/side/side.ts
+++ src/app/components/side/side.ts
@@ -1,5 +1,6 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Recipe } from '../../recipe.model';
 
 @Component({
   standalone: true,
@@ -9,10 +10,11 @@ import { CommonModule } from '@angular/common';
   styleUrl: './side.css'
 })
 export class Side {
-  @Input() recipes: any[] = [];
+  @Input() recipes: Recipe[] = [];
   @Output() recipeSelected = new EventEmitter<string>();
 
   selectRecipe(id: string) {
     this.recipeSelected.emit(id);
+    
   }
 }
